6bc4526aae1718b6fa90927100d8a13d3bc7bb33
Atharva <atharvakharate@gmail.com>
2025-09-30 14:37:27 +0530

    new refresh button

--- src/components/WineOcrWizard.tsx
+++ src/components/WineOcrWizard.tsx
@@ -104,7 +104,7 @@ export const WineOcrWizard: React.FC = () => {
         <div>
           <button
             onClick={refreshShopify}
-            disabled={refreshing}
+            disabled={refreshing || ocrStarted}
             className="flex items-center space-x-2 px-3 py-1 bg-green-600 text-white rounded hover:bg-green-700 disabled:opacity-50"
           >
             {refreshing ? (
